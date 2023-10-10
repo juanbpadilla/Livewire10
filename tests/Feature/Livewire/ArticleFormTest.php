9939b80e6b9980bf7ad29d8d1bec28fe2b084834
juanbpadilla <juanbpadillac@gmail.com>
2023-10-10 02:10:36 -0400

    Cómo testear componentes de blade de Livewire

--- tests/Feature/Livewire/ArticleFormTest.php
+++ tests/Feature/Livewire/ArticleFormTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Livewire;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,6 +12,29 @@ use Tests\TestCase;
 class ArticleFormTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    function article_form_renders_properly()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->get(route('articles.create'))
+            ->assertSeeLivewire('article-form');
+
+        $article = Article::factory()->create();
+
+        $this->actingAs($user)->get(route('articles.edit', $article))
+            ->assertSeeLivewire('article-form');
+    }
+
+    /** @test */
+    function blade_template_is_wired_properly()
+    {
+        Livewire::test('article-form')
+            ->assertSeeHtml('wire:submit="save"')
+            ->assertSeeHtml('wire:model.live="article.title"')
+            ->assertSeeHtml('wire:model.live="article.content"');
+    }
 
     /** @test */
     function can_create_new_articles()
@@ -28,12 +53,33 @@ class ArticleFormTest extends TestCase
     }
 
     /** @test */
+    function can_update_articles()
+    {
+        $article = Article::factory()->create();
+
+        Livewire::test('article-form', ['article' => $article])
+            ->assertSet('article.title', $article->title)
+            ->assertSet('article.content', $article->content)
+            ->set('article.title', 'Updated title')
+            ->call('save')
+            ->assertSessionHas('status')
+            ->assertRedirect(route('articles.index'));
+
+        $this->assertDatabaseCount('articles', 1);
+
+        $this->assertDatabaseHas('articles', [
+            'title' => 'Updated title'
+        ]);
+    }
+
+    /** @test */
     function title_is_required()
     {
         Livewire::test('article-form')
             ->set('article.content', 'Article content')
             ->call('save')
             ->assertHasErrors(['article.title' => 'required'])
+            ->assertSeeHtml(__('validation.required', ['attribute' => 'title']));
         ;
     }
 
@@ -45,6 +91,10 @@ class ArticleFormTest extends TestCase
             ->set('article.content', 'Article content')
             ->call('save')
             ->assertHasErrors(['article.title' => 'min'])
+            ->assertSeeHtml(__('validation.min.string', [
+                'attribute' => 'title',
+                'min' => 4
+            ]));
         ;
     }
 
@@ -55,6 +105,7 @@ class ArticleFormTest extends TestCase
             ->set('article.title', 'New Article')
             ->call('save')
             ->assertHasErrors(['article.content' => 'required'])
+            ->assertSeeHtml(__('validation.required', ['attribute' => 'content']));
         ;
     }
 
