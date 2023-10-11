255b778e50e4f8f8bb27f6ff648dece383446af5
juanbpadilla <juanbpadillac@gmail.com>
2023-10-11 05:38:14 -0400

    Estilizando el selector de imágenes con Tailwind

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -14,9 +14,22 @@
             </x-slot>
 
             <x-slot name="form">
-                <div class="col-span-6 sm:col-span-4">
-                    <x-label for="image" :value="__('Image')"/>
-                    <x-input wire:model.live="image" id="image" class="mt-1 block w-full" type="file" />
+                <div class="col-span-6 sm:col-span-4 relative">
+                    @if($image)
+                        <x-danger-button wire:click="$set('image', null)" class="absolute bottom-2 right-2">
+                            {{ __('Change Image') }}
+                        </x-danger-button>
+                        <img src="{{ $image->temporaryUrl() }}" class="border-2 rounded" alt="">
+                    @elseif($article->image)
+                        <x-label for="image" :value="__('Change Image')" class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"/>
+
+                        <img src="{{ Storage::disk('public')->url($article->image) }}" alt="">
+                    @else
+                        <div class="h-32 bg-gray-50 dark:bg-gray-900 border-2 border-dashed flex items-center justify-center">
+                            <x-label for="image" :value="__('Select Image')" class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"/>
+                        </div>
+                    @endif
+                    <x-input wire:model.live="image" id="image" class="hidden" type="file" />
                     <x-input-error for="image" class="mt-2" />
                 </div>
                 <div class="col-span-6 sm:col-span-4">
