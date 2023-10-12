59e92f58f542eb5b07c97a93bb3e7362a92bd331
juanbpadilla <juanbpadillac@gmail.com>
2023-10-12 01:18:15 -0400

    Validación de relaciones con Livewire

--- tests/Feature/Livewire/ArticleFormTest.php
+++ tests/Feature/Livewire/ArticleFormTest.php
@@ -202,6 +202,32 @@ class ArticleFormTest extends TestCase
     }
 
     /** @test */
+    function category_is_required()
+    {
+        Livewire::test('article-form')
+            ->set('article.title', 'New Article')
+            ->set('article.slug', 'new article')
+            ->set('article.content', 'Article content')
+            ->set('article.category_id', null)
+            ->call('save')
+            ->assertHasErrors(['article.category_id' => 'required'])
+            ->assertSeeHtml(__('validation.required', ['attribute' => 'category id']));
+    }
+
+    /** @test */
+    function category_must_exist_in_database()
+    {
+        Livewire::test('article-form')
+            ->set('article.title', 'New Article')
+            ->set('article.slug', 'new article')
+            ->set('article.content', 'Article content')
+            ->set('article.category_id', 1)
+            ->call('save')
+            ->assertHasErrors(['article.category_id' => 'exists'])
+            ->assertSeeHtml(__('validation.exists', ['attribute' => 'category id']));
+    }
+
+    /** @test */
     function slug_must_be_unique()
     {
         $article = Article::factory()->create();
